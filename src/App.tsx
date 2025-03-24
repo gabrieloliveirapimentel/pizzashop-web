1461218a6ea148d2bcfa444a2e1d2753a6389586
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-03-24 17:14:35 -0300

    Layouts da Aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,18 @@ import "./index.css";
 import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 
+import { AuthLayout } from "./pages/__layouts/auth";
+import { AppLayout } from "./pages/__layouts/app";
+
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/sign-in" element={<SignIn />} />
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignIn />} />
+      </Route>
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 }
