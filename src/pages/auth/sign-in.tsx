f15822d1ce54e5aa4c47e17d414ff006c0286149
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-03-25 10:48:19 -0300

    lint --fix

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Helmet } from "react-helmet-async";
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Helmet } from 'react-helmet-async'
 
 export function SignIn() {
   return (
@@ -29,5 +29,5 @@ export function SignIn() {
         </div>
       </div>
     </>
-  );
+  )
 }
