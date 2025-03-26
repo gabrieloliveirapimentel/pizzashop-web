b447cb4538ac554897355378be1a8f386c01dabb
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-03-26 10:44:01 -0300

    Componente: Detalhes do pedido

--- src/pages/app/orders/components/order-table-row.tsx
+++ src/pages/app/orders/components/order-table-row.tsx
@@ -1,15 +1,23 @@
 import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableRow, TableCell } from '@/components/ui/table'
 import { Search, ArrowRight, X } from 'lucide-react'
+import { OrderDetails } from './order-details'
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="sm">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="sm">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         0195d26a-d9c4-7e70-bfea-f641aceebf89
