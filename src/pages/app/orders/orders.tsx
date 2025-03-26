b130b171075c6570fe1ad126345c74d6db32d7f6
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-03-26 10:06:58 -0300

    Componente: Paginação

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet-async'
 import { OrderTableFilters } from './components/order-table-filters'
 import { OrderTableHeader } from './components/order-table-header'
 import { OrderTableRow } from './components/order-table-row'
+import { Pagination } from '@/components/pagination'
 
 export function Orders() {
   return (
@@ -11,18 +12,21 @@ export function Orders() {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderTableFilters />
-        <div className="border rounded-md">
-          <Table>
-            <OrderTableHeader />
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return <OrderTableRow key={i} />
-              })}
-            </TableBody>
-          </Table>
+
+        <div className="space-y-2.5">
+          <OrderTableFilters />
+          <div className="border rounded-md">
+            <Table>
+              <OrderTableHeader />
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrderTableRow key={i} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
